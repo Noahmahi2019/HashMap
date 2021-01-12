912570bc0f6cc9d420880b0a925682793c9aff30
Noah <mahinoah_2019@yahoo.com>
2021-01-11 19:45:33 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String, String> user = new HashMap<String, String>();
+        user.put("jane@gmail.com", "Jane");
+        user.put("eyre@gmail.com", "Eyre");
+        user.put("george@gmail.com", "George");
+        user.put("Godwin@gmail.com", "Godwin");
+        
+        System.out.println("user : " + user.get("jane@gmail.com"));
+        System.out.println("user : " + user.get("eyre@gmail.com"));
+        System.out.println("user : " + user.get("george@gmail.com"));
+        System.out.println("user : " + user.get("Godwin@gmail.com"));
+
+
+        System.out.println(user.put("newUser@gmail.com","new user"));
+        System.out.println(user.get("newUser@gmail.com"));
+
+        user.remove("Godwin@gmail.com");
+        System.out.println(user.get("Godwin@gmail.com"));
+    }
+}
